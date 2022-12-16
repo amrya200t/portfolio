70ee7d6f5ef53cffa512b2a1c315d4896e9c8584
Amr <amrya200t@gmail.com>
2022-12-16 10:49:02 +0200

    reset

--- src/App.js
+++ src/App.js
@@ -4,15 +4,6 @@ import Routes from "./Routes/Routes";
 import ScrollUpButton from "./Components/ScrollUpButton";
 
 function App() {
-  fetch("https://fair-tan-gosling-garb.cyclic.app/api/v1/skills", {
-    headers: {
-      Authorization: `Basic YXBpX3dob2FtaV9hbXI6Y3FYVmVZSHchVm5nNkcx`,
-    },
-    method: "GET",
-  })
-    .then((data) => console.log(data))
-    .catch((error) => console.log(error));
-
   return (
     <>
       {/* NAVIGATION BAR */}
